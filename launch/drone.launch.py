6d3decb586039173a8c2a9e60d4d793c3ea89ad8
Siva2002-baka <saikumar14970@gmail.com>
2026-01-23 23:09:57 +0800

    first commit

---
+++ launch/drone.launch.py
@@ -0,0 +1,115 @@
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.substitutions import Command, LaunchConfiguration
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    urdf_name = 'drone.xacro'
+    urdf = os.path.join(get_package_share_directory('drone'), 'urdf', urdf_name)
+    control_yaml_file = os.path.join(get_package_share_directory(
+        'drone'
+    ), 'config', 'ros2_control.yaml')
+    robot_desc = ParameterValue(
+        Command(['xacro ', urdf, ' ', 'ros2_control_yaml:=', control_yaml_file]),
+        value_type=str
+    )
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) if true',
+    )
+    gz_sim_share = get_package_share_directory('ros_gz_sim')
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(gz_sim_share, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': f'-r empty.sdf'
+        }.items()
+
+    )
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'drone',
+            '-allow-renaming', 'true',
+            '-z', '0.5',
+            # '-R', '3.14',
+           
+        ]
+
+    )
+
+    gz_ros2_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+        ]
+    )
+    params = {
+        'use_sim_time': use_sim_time,
+        'robot_description': robot_desc,
+    }
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[params],
+        arguments=[],
+    )
+
+    spawner_jsb = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager',
+            '/controller_manager',
+        ],
+        output='screen',
+    )
+
+    spawner_rotor_velocity_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'rotor_velocity_controller',
+            '--controller-manager',
+            '/controller_manager',
+        ],
+        output='screen',
+    )
+    wait_for_control_manager = Node(
+        package='drone',
+        executable='wait_for_controller_manager',
+        output='screen',
+    )
+    spawn_after_controller_manager = RegisterEventHandler(
+        OnProcessExit(
+            target_action=wait_for_control_manager,
+            on_exit=[
+                spawner_jsb,
+                spawner_rotor_velocity_controller,
+            ],
+        ),
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(gz_sim)
+    ld.add_action(gz_spawn_entity)
+    ld.add_action(gz_ros2_bridge)
+    ld.add_action(wait_for_control_manager)
+    ld.add_action(spawn_after_controller_manager)
+    ld.add_action(start_robot_state_publisher_cmd)
+
+    return ld
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'drone'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='siva',
+    maintainer_email='saikumar14970@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'wait_for_controller_manager = quadruped_drone.wait_for_controller_manager:main',
+        ],
+    },
+)
